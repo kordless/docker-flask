b5278e7559ef06de8b452ad0c4242fef909af10d
Kord Campbell <kord@Buddhas-Butt.local>
2017-10-11 15:01:07 -0700

    add search

--- app/hello.py
+++ app/hello.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def hello():
     return render_template('hello.html')
 
+@app.route('/search')
+def hello():
+    return render_template('search.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
